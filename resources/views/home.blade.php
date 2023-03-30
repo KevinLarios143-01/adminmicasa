c93fb65679ea577cb8175c2150cf5c9b73a23163
KLARIOS <alexander.larios@galileo.edu>
2023-03-30 10:21:18 -0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            {{ __('You are logged in!') }}
+            <example-component/>
+    </div>
+</div>
+@endsection
